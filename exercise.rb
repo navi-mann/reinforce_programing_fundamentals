ce7432d616d8829d0ebbc2022b3ccf058d15d526
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-05 19:06:39 -0400

    fixed the stupid bug

--- exercise.rb
+++ exercise.rb
@@ -1,13 +1,14 @@
 hash = {}
 (1..50).each do |num|
+
   if num % 2 == 0
-    hash[num.to_s] = (num + 1)
+    hash[num] = (num + 1)
   elsif num % 7 == 0
-    hash[num.to_s] = (num - 1)
+    hash[num] = (num - 1)
   elsif num % 2 == 0 && num % 7 == 0
-    hash[num.to_s] = (num * 2)
+    hash[num] = (num * 2)
   else
-    hash[num.to_s] = num
+    hash[num] = num
   end
-  puts hash
 end
+puts hash
